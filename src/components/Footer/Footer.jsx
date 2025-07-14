d9bd80054afc3426ea421e42f3e1725275947b7a
weammahmoud00@gmail.com <weammahmoud00@gmail.com>
2025-07-14 21:10:16 +0200

    last

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,50 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <footer className="flex flex-col space-y-10 justify-center m-10">
+       
+        <div className="flex justify-center space-x-5">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+          </a>
+          <a
+            href="https://messenger.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+          </a>
+        </div>
+        <p className="text-center text-gray-700 font-medium">
+          © 2022 Company Ltd. All rights reservered.
+        </p>
+      </footer>
+    </>
+  );
 }
